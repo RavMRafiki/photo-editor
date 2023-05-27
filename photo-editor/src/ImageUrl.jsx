35f7dd6918b6ba5cff88ba6eccf85a82eecfa7bd
RavMRafiki <98659406+RavMRafiki@users.noreply.github.com>
2023-05-27 18:12:56 +0200

    Added changing value

--- photo-editor/src/ImageUrl.jsx
+++ photo-editor/src/ImageUrl.jsx
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 
 export default function ImageUrl() {
   const [url, setUrl] = useState("https://source.unsplash.com/EwKXn5CapA4");
+
+  function handleChange(e) {
+    setUrl(e.target.value);
+  }
+
   return (
     <div>
-      <input className="url" type="text" value={url} onChange={""} />
+      <input className="url" type="text" value={url} onChange={handleChange} />
     </div>
   );
 }
